ac14fd17c44b84d7513c3590806cab736c77915b
adayc <adayc@hotmail.com>
2017-11-11 07:07:52 -0500

    Ejecutando animación

--- js/app.js
+++ js/app.js
@@ -1,15 +1,76 @@
+// Variables 
+var colums = document.getElementById('columns');
+var cells = [['', '', ''], ['', '', ''], ['', '', '']];
 
-// Declaración de variables
-var board = document.querySelector('.board-js');
-document.getElementById('c7').style.backgroundColor = 'red';
 
-// Declaramos las funciones en el momento en que se carga la ventana del navegador
 window.onload = function() {
-  board.addEventListener('onkeypress', addColor);
-};
+  // Creación de las clases con array - etiqueta div 
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      var newElement = document.createElement('div');
+      newElement.id = 'cells[' + i + '][' + j + ']';
+      newElement.className = 'cell';
+      newElement.innerHTML = 'cells[' + i + '][' + j + ']';
+      columns.appendChild(newElement);
+    }
+  }
 
+  var i = 1;
+  var j = 1;
+    document.getElementById('cells[' + i + '][' + j + ']').classList.add('red');
+ 
 
-function addColor(event) {
-  document.getElementById('c6').style.backgroundColor = 'red';
+  window .addEventListener('keyup', function(e) {
+ 
   
+    var ascii = e.keyCode;
+    console.log(ascii)
+    if (ascii == 38) { // up
+      i=i-1;
+      j=j; 
+      if (i>=0 && j>=0 && i<=2) { 
+      document.getElementById('cells[' + (i +1) + '][' + j + ']').classList.remove('red');
+      document.getElementById('cells[' + i  + '][' + j + ']').classList.add('red');
+      }
+
+    }
+
+    if (ascii == 40) { // down
+      i=i+1;
+      j=j;
+
+      if (i>=0 && j>=0 && i<=2) { 
+        document.getElementById('cells[' + (i -1) + '][' + j + ']').classList.remove('red');
+        document.getElementById('cells[' + i  + '][' + j + ']').classList.add('red');
+        }
+      
+
+    }
+
+    if (ascii == 37) { // left
+      i=i;
+      j=j-1;
+
+      if (i>=0 && j>=0 && j<=2) { 
+        document.getElementById('cells[' + i  + '][' + (j +1) + ']').classList.remove('red');
+        document.getElementById('cells[' + i  + '][' + j + ']').classList.add('red');
+        }
+
+    }
+
+    if (ascii == 39) { // right
+      i=i;
+      j=j+1;
+      
+      if (i>=0 && j>=0 && j<=2) { 
+        document.getElementById('cells[' + i  + '][' + (j - 1) + ']').classList.remove('red');
+        document.getElementById('cells[' + i  + '][' + j + ']').classList.add('red');
+        }
+
+      
+
+    }
+
+  });
 };
+ 
